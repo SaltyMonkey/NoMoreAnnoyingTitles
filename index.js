6354fabdead0dcd576c9a27e86bbb741b31b1d9a
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2018-08-09 22:25:18 +0300

    initial version

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = function NoMoreAnnoyingTitles(mod) {
+	const achieves = {
+		99115: true,
+		99116: true,
+		99111: true,
+		99112: true,
+		99113: true,
+		99114: true,
+		99103: true,
+		99104: true,
+		99105: true,
+		99106: true,
+		99103: true,
+		99102: true,
+		99101: true
+	}
+	
+	mod.hook('S_SPAWN_USER', 13, event=>{
+		if(achieves[event.title]) {
+			event.title = 0;
+			return true;
+		}
+	});
+}
